92b752f8f1addd83b78339657a6d4a1cc1f4a635
Courressa <courressamalcolm2030@gmail.com>
2026-06-19 05:44:47 -0400

    Setup filtering options to be passed based on what was selected

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -2,67 +2,63 @@ import { useState } from "react";
 import { categories, moods } from "../../data/data";
 
 const mapOptions = (options) => {
-  return options.map(option => {
-    return <option key={option} value={option}>{option}</option>;
-  }) 
+    return options.map(option => {
+        return <option key={option} value={option}>{option.charAt(0).toUpperCase() + option.slice(1)}</option>;
+    }) 
 }
 
-export default function Filter() {
-  const [type, setType] = useState("all");
-  const [categorySelected, setCategorySelected] = useState("");
-  const [moodSelected, setMoodSelected] = useState("");
-  const [hasVideo, setHasVideo] = useState(true);
-
-  export const filtered = {
-    type: type,
-    category: categorySelected,
-    mood: moodSelected,
-    hasVideo: hasVideo
-  }
-
+export default function Filter({
+    type, setType,
+    categorySelected, setCategorySelected,
+    moodSelected, setMoodSelected,
+    hasVideo, setHasVideo
+}) {
   return (
     <div className="filter">
-      <form>
-        <label>Type </label>
-        <select
-            name="type"
-            id="type" 
-            onChange={(e) => setType(e.target.value)}
-        >
-            <option value="all">All</option>
-            <option value="individual">Individual</option>
-            <option value="collection">Collection</option>
-        </select>
+        <form>
+            <label>Type </label>
+            <select
+                name="type"
+                id="type" 
+                value={type}
+                onChange={(e) => setType(e.target.value)}
+            >
+                <option value="all">All</option>
+                <option value="individual">Individual</option>
+                <option value="collection">Collection</option>
+            </select>
 
-        <label>Categories </label>
-        <select
-            name="categories"
-            id="categories"
-            onChange={(e) => setCategorySelected(e.target.value)}
-        >
-            <option value=""></option>
-            {mapOptions(categories)}
-        </select>
+            <label>Categories </label>
+            <select
+                name="categories"
+                id="categories"
+                value={categorySelected}
+                onChange={(e) => setCategorySelected(e.target.value)}
+            >
+                <option value=""></option>
+                {mapOptions(categories)}
+            </select>
 
-        <label>Moods </label>
-        <select
-            name="moods"
-            id="moods"
-            onChange={(e) => setMoodSelected(e.target.value)}
-        >
-            <option value=""></option>
-            {mapOptions(moods)}
-        </select>
+            <label>Moods </label>
+            <select
+                name="moods"
+                id="moods"
+                value={moodSelected}
+                onChange={(e) => setMoodSelected(e.target.value)}
+            >
+                <option value=""></option>
+                {mapOptions(moods)}
+            </select>
 
-        <label>Has Video</label>
-        <input
-          type="checkbox"
-          id="hasVideo"
-          name="hasVideo"
-          checked={hasVideo}
-          onChange={(e) => setHasVideo(e.target.checked)}
-        />
-      </form>
+            <label>Has Video</label>
+            <input
+            type="checkbox"
+            id="hasVideo"
+            name="hasVideo"
+            checked={hasVideo}
+            onChange={(e) => setHasVideo(e.target.checked)}
+            />
+        </form>
     </div>
   )
 }
--- src/pages/BrowsePage.jsx
+++ src/pages/BrowsePage.jsx
@@ -1,10 +1,31 @@
-import Filter from "../components/filter/filter"
+import { useState } from "react";
+import Filter from "../components/filter/filter";
 
 export default function BrowsePage() {
+    const [type, setType] = useState("all");
+    const [categorySelected, setCategorySelected] = useState("");
+    const [moodSelected, setMoodSelected] = useState("");
+    const [hasVideo, setHasVideo] = useState(true);
+
   return (
     <div className="page browse">
       <h1>Browse Wallpapers</h1>
-      <Filter />
+      <Filter 
+        type={type}
+        setType={setType}
+        categorySelected={categorySelected}
+        setCategorySelected={setCategorySelected}
+        moodSelected={moodSelected}
+        setMoodSelected={setMoodSelected}
+        hasVideo={hasVideo}
+        setHasVideo={setHasVideo}
+      />
+      <div className="filtered">
+        <p>Filtered by: {type}</p>
+        <p>Category: {categorySelected}</p>
+        <p>Mood: {moodSelected}</p>
+        <p>Has Video: {hasVideo ? "Yes" : "No"}</p>
+      </div>
     </div>
   )
 }
